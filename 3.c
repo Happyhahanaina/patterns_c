348f6271f9402a5917fdee3e3851c3b64e82cf4a
moments <163236324+Happyhahanaina@users.noreply.github.com>
2025-07-11 15:57:09 +0530

    Add files via upload
    This will make a right angle triangle of stars
    for example
    **********
    ********* 
    ********  
    *******   
    ******    
    *****     
    ****      
    ***       
    **        
    *

---
+++ 3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for(int i = 0; i<n; i++){
+        for  (int j = n; j >i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// This will make a right angle triangle of stars
+// for example
+// **********
+// ********* 
+// ********  
+// *******   
+// ******    
+// *****     
+// ****      
+// ***       
+// **        
+// *
